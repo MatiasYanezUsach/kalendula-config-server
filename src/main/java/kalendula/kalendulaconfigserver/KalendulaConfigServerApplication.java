3f4c7757410460824edc9f6a234eeee08dc42f06
Matias Yanez <matias.yanez@usach.cl>
2023-10-07 19:59:01 -0300

    subida archivos

---
+++ src/main/java/kalendula/kalendulaconfigserver/KalendulaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package kalendula.kalendulaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalendulaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalendulaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kalendula/kalendulaconfigserver/KalendulaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kalendula.kalendulaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalendulaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
